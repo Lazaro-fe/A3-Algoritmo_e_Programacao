348738bfc0cce8585ca8143502cecaf5c6999f0b
Lázaro Ferreira <155861352+Lazaro-fe@users.noreply.github.com>
2026-05-31 16:11:41 +0000

    Inicio de black-jack e comentar os outros códigos

--- Black_Jack.java
+++ Black_Jack.java
@@ -1,3 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Black_Jack {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random sortear_cartas = new Random();
+
+        int [][] cartas_presentes_no_baralho = new int[52][2];
+        
+    }
 }
